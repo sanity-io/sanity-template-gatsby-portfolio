e2b5cb3b9b2338d8202beba2342fe0be76a3166f
Knut Melvær <knut.melvaer@gmail.com>
2019-05-03 12:53:24 +0200

    require alt text

--- template/studio/schemas/objects/figure.js
+++ template/studio/schemas/objects/figure.js
@@ -18,6 +18,7 @@ export default {
       name: 'alt',
       type: 'string',
       title: 'Alternative text',
+      validation: Rule => Rule.error('You have to fill out the alternative text.').required(),
       description:
         'Important for SEO and accessiblity.',
       options: {
