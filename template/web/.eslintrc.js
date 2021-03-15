bd76976e98df42320ef80798c300df487bfa3221
Bjørge Næss <bjoerge@gmail.com>
2021-03-15 20:59:36 +0100

    chore: cleanup eslint config + deps

--- template/web/.eslintrc.js
+++ template/web/.eslintrc.js
@@ -1,13 +1,29 @@
 module.exports = {
-  extends: ['standard', 'standard-react', 'plugin:import/errors', 'plugin:import/warnings'],
-  rules: {
-    'react/prop-types': 0,
-    'object-curly-spacing': ['error', 'never']
+  parser: "babel-eslint",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true
+    }
   },
+  env: {
+    node: true,
+    browser: true
+  },
+  rules: {
+    "react/prop-types": "off"
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended"
+  ],
   settings: {
     react: {
-      pragma: 'React',
-      version: '16.8.4'
+      pragma: "React",
+      version: "detect"
     }
   }
-}
+};
