101feb616f8e9868f59498322918092cc40fdf75
kathmbeck <katherine.murphy.beck@gmail.com>
2021-11-22 22:47:43 -0800

    extracted gatsbyPreviewAction to npm package

--- template/studio/resolveDocumentActions.js
+++ template/studio/resolveDocumentActions.js
@@ -3,7 +3,7 @@
 // import the default document actions
 import defaultResolve from "part:@sanity/base/document-actions";
 
-import { gatsbyPreviewAction } from "./gatsbyPreviewAction";
+import { gatsbyPreviewAction } from "sanity-plugin-gatsby-cloud-preview";
 
 export default function resolveDocumentActions(props) {
   return [...defaultResolve(props), gatsbyPreviewAction];
