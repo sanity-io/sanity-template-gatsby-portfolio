5becdca63b08650f225cf49749d24b97604333e4
kathmbeck <katherine.murphy.beck@gmail.com>
2021-12-20 08:47:28 -0800

    update watchMode default config

--- template/web/gatsby-config.js
+++ template/web/gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       options: {
         ...clientConfig.sanity,
         token,
-        watchMode: !previewEnabled,
+        watchMode: !isProd && !previewEnabled,
         overlayDrafts: (!isProd || previewEnabled) && token,
       }
     }
